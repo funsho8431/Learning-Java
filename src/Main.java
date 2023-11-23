3418f1925cabee30c52087f6433a19e91cd24fc5
Salami Lateef Funsho <forfunsho@Salamis-MacBook-Pro.local>
2023-11-23 13:36:02 -0500

    Mortgage Calculator 1st Version

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Coding a Mortgage Calculator
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Principal: ");
+        int principal = scanner.nextInt();
+        System.out.println("Rate: ");
+        float rate = scanner.nextFloat();
+        float toPercentage = rate / 100;
+        float monthlyInterestRate = toPercentage / 12;
+        System.out.println("Period (Years): ");
+        int years = scanner.nextInt();
+        float totalNumberOfPayments = 12 * years;
+        System.out.println("Mortgage");
+
+        float top = (float) (monthlyInterestRate * Math.pow((1 + monthlyInterestRate), (totalNumberOfPayments)));
+        float bottom = (float) Math.pow((1 + monthlyInterestRate), (totalNumberOfPayments)) -1;
+
+        float mortgage = principal * (top / bottom);
+
+        System.out.println(mortgage);
+
+    }
+}
